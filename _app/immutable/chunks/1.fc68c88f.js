90216db3356ceccce104c195d425f7be57af51df
Eksno <33558637+Eksno@users.noreply.github.com>
2023-03-10 08:39:05 +0000

    Deploying to gh-pages from @ Futino/web-template@8c1aa551f86a2a450dc6795e6a69343d0a35b31e 🚀

---
+++ _app/immutable/chunks/1.fc68c88f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.00b7f352.js";export{t as component};
---
+++ _app/immutable/chunks/2.cc345341.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.92a370e7.js";export{t as component};
